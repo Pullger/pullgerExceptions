cad440ada23daf8419f094bb128f1020c865458e
NeoUKR <neokkv@gmail.com>
2022-08-29 15:08:45 +0300

    Init

---
+++ __init__.py
@@ -0,0 +1,20 @@
+try:
+    from pullgerReflection.exceptions import *
+except:
+    pass
+# ----------------------------
+try:
+    from pullgerDomain.exceptions import *
+except:
+    pass
+# ----------------------------
+try:
+    from pullgerSquirrel import exceptions as pullgerSquirrel
+except:
+    pass
+
+
+# from .connections  import *
+# from .sqirrel import *
+# from .multisessionManager import *
+# from .domains import *
